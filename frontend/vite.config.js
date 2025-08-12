ef3ec091ac9799273be3eca7f7d4868d42fab71d
freakynutzz100 <freetimerenderzz100@gmail.com>
2025-08-12 22:44:22 +0530

    message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': '/src'
-        }
-    },
-    build: {
-        commonjsOptions: {
-            transformMixedEsModules: true
-        }
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'
     }
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    },
+    outDir: 'dist',
+    sourcemap: true
+  },
+  base: '/'
 });
